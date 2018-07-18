d69cde8082485886b249922e19b2b9071a1e7982
NatNat180 <Nathan.Argall@gmail.com>
2018-07-18 08:33:05 -0500

    Update Ramp.cs
    Restrict movement of shooter arm

--- coin pusher/Assets/Scripts/Ramp.cs
+++ coin pusher/Assets/Scripts/Ramp.cs
@@ -12,8 +12,12 @@ public class Ramp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// Get position of mouse and orient it from world space to screen space
 		Vector3 mousePositionVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
 		mousePositionVector3 = Camera.main.WorldToScreenPoint(mousePositionVector3) / 50f;
+		// Restrict movement of shooter arm
+		mousePositionVector3.x = Mathf.Clamp(mousePositionVector3.x, 0, Screen.width - 10);
+		// Produce target direction from mouse pos and rotate shooter arm toward that target 
 		Vector3 targetDirection = mousePositionVector3 - transform.position;
 		shooterArm.rotation = Quaternion.LookRotation(Vector3.up, -targetDirection);
 	}
