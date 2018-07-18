567125b60957ec30aa74626f6779b2f96cb053ab
NatNat180 <Nathan.Argall@gmail.com>
2018-07-17 20:58:09 -0500

    add ramp script

---
+++ coin pusher/Assets/Scripts/Ramp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ramp : MonoBehaviour {
+
+	public Collider rampCollider;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Mouse X") < 0) {
+			Debug.Log("Mouse moved left");
+		}
+		if (Input.GetAxis("Mouse X") > 0) {
+			Debug.Log("Mouse moved right");
+		}
+	}
+}
