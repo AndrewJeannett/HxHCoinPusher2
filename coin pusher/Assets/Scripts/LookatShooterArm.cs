0ce7be295d51a2a7db4b3bd57130148b7176739a
AndrewJeannett <andrewjeannett@gmail.com>
2018-09-01 12:09:57 -0500

    shooter arm fixed sorta

---
+++ coin pusher/Assets/Scripts/LookatShooterArm.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookatShooterArm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray mouseray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float midpoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
+
+
+        transform.LookAt(mouseray.origin + mouseray.direction * midpoint);
+	}
+}
--- coin pusher/Assets/Scripts/Ramp.cs
+++ coin pusher/Assets/Scripts/Ramp.cs
@@ -12,13 +12,13 @@ public class Ramp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Get position of mouse, restrict x movement, and orient it from world space to screen space
-		Vector3 mousePositionVector3 = new Vector3(
-			Mathf.Clamp(Input.mousePosition.x, 0, 15f), 0, 0);
-		mousePositionVector3 = Camera.main.WorldToScreenPoint(mousePositionVector3) / 75f;
-		// Produce target direction from mouse pos and rotate shooter arm toward that target 
-		Vector3 targetDirection = mousePositionVector3 - transform.position;
-		Quaternion rotation = Quaternion.LookRotation(Vector3.up, -targetDirection);
-		shooterArm.rotation = Quaternion.Slerp(shooterArm.rotation, rotation, Time.deltaTime * 2);
-	}
+        // Get position of mouse, restrict x movement, and orient it from world space to screen space
+        Vector3 mousePositionVector3 = new Vector3(
+            Mathf.Clamp(Input.mousePosition.x, 0, 15f), 0, 0);
+        mousePositionVector3 = Camera.main.WorldToScreenPoint(mousePositionVector3) / 75f;
+        // Produce target direction from mouse pos and rotate shooter arm toward that target 
+        Vector3 targetDirection = mousePositionVector3 - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(Vector3.up, -targetDirection);
+        shooterArm.rotation = Quaternion.Slerp(shooterArm.rotation, rotation, Time.deltaTime * 2);
+    }
 }
